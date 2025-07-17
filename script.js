883b10b6d223042d33a2079c49ddc92f768408b8
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-07-17 01:59:24 -0400

    Finish creating entirety of HTML structure Create basic CSS styling and page layout for game Create function to instruct user on how to play

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 const input = document.getElementById("input");
 const submitBtn = document.getElementById("submitBtn");
 const clearBtn = document.getElementById("clearBtn")
-const promptText = document.getElementById("prompt");
+const promptText = document.getElementById("promptText");
 const canvas = document.getElementById("canvas");
+const gridText = document.getElementById("gridText");
 const numRows = 4;
 const numCols = 4;
 
 
-// on page load, create prompt to guide user to use the input feature.
-function gridPrompt(){
+// on page load make sure the default grid is 10 x 10
+
+
+//On input focus, prompt user to choose a grid size
+input.addEventListener('focus', () =>{
     promptText.textContent = "Please enter a number between 2 and 64."
-}
-gridPrompt();
+});
 
-// create function that, if user enters number outside 2-64, prompts
-// them to re-enter a valid number within the specified range
-
-
-// create a function that takes whatever value they enter in the input
-// and creates a p tag next to the input to show it will be a 16x16 grid, for example.
-
-
-
-
-
+//On click of submit button, create grid or inform user their input is invalid
+submitBtn.addEventListener('click', () =>{
+    const inputValue = input.value;
+    if(inputValue >= 2 && inputValue <= 64){
+        //CALL YOUR MAKEGRID FUNCTION HERE
+        promptText.textContent = `Generating a ${inputValue} x ${inputValue} grid...`
+    } else{
+        promptText.textContent = "Invalid number. Please enter a number between 2 and 64."
+    }
+});
 
 
-
-// const numbers = prompt("how many?");
-// const inputElement = document.getElementById("input");
-// const inputValue = inputElement.value;
 
 // add event listener for enter or click of button next to input to run function
 
@@ -51,6 +49,20 @@ function createRows(numRows, numCols){
 }
 
 createRows(numRows, numCols);
+
+
+
+function draw(){
+// add event listeners to every square for mousedown and mouseover events
+// do so by adding an event listener to every square targetting their class
+}
+
+
+
+
+
+
+
 
 
 
@@ -77,4 +89,3 @@ createRows(numRows, numCols);
 
 
 
-// add event listeners to every square for mousedown and mouseover events
