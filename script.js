91714c1371cd0775384980ed398be4cb17fa4acf
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-07-20 16:29:34 -0400

    Simplify submit button and event listener Debug makeGrid function and fix input problems Fix and complete makeGrid function to return properly

--- script.js
+++ script.js
@@ -3,52 +3,59 @@ const submitBtn = document.getElementById("submitBtn");
 const clearBtn = document.getElementById("clearBtn")
 const promptText = document.getElementById("promptText");
 const canvas = document.getElementById("canvas");
-const gridText = document.getElementById("gridText");
+// const gridText = document.getElementById("gridText");
 
 
 // on page load make sure the default grid is 10 x 10
-
 makeGrid();
 
 //On input focus, prompt user to choose a grid size
-input.addEventListener('focus', () =>{
+input.addEventListener('focus', () => {
     promptText.textContent = "Please enter a number between 2 and 64."
 });
 
 //On press of 'enter' key, create grid or inform user their input is invalid
-input.addEventListener('keydown', function (e){
-    const inputValue = input.value;
-    if(e.key === "Enter"){
-        if(inputValue >= 2 && inputValue <= 64){
-        makeGrid();
-        promptText.textContent = `Generating a ${inputValue} x ${inputValue} grid...`
-    } else{
-        promptText.textContent = "Invalid number. Please enter a number between 2 and 64."
-    }
-    }
-});
+// input.addEventListener('keydown', function (e){
+//     const inputValue = input.value;
+//     if(e.key === "Enter"){
+//         makeGrid();
+//         if(inputValue >= 2 && inputValue <= 64){
+//             promptText.textContent = `Generating a ${inputValue} x ${inputValue} grid...`
+//         } 
+//         else{
+//             promptText.textContent = "Invalid number. Please enter a number between 2 and 64."
+//         }
+//     }
+// });
 
 // On click of submit button, create grid or inform user their input is invalid
-submitBtn.addEventListener('click', () =>{
-    const inputValue = input.value;
-    if(inputValue >= 2 && inputValue <= 64){
-        makeGrid();
-        promptText.textContent = `Generating a ${inputValue} x ${inputValue} grid...`
-    } else{
-       
-    }
-});
+submitBtn.addEventListener('click', makeGrid);
+
+// submitBtn.addEventListener('click', () =>{
+//     const inputValue = input.value;
+//     makeGrid();
+//     if(inputValue >= 2 && inputValue <= 64){
+//         promptText.textContent = `Generating a ${inputValue} x ${inputValue} grid...`
+//     } else{
+//         promptText.textContent = "Invalid number. Please enter a number between 2 and 64."
+//     }
+// });
+
+
 
 
 //make the grid
 function makeGrid(){
     let number = input.value;
-    if(isNaN(number) || number < 2 || number > 64){
-        promptText.textContent = "Invalid number. Please enter a number between 2 and 64."
+    if(number < 0 || number > 64 || isNaN(number)){
+        promptText.textContent = `Invalid number. Generating 10 x 10 grid. Please enter a number between 2 and 64.`;
     } else {
         promptText.textContent = "";
+        input.value = "";
+        canvas.innerHTML = "";
     }
     if(number == 0 || number > 64 || number == ""){
+        canvas.innerHTML = "";
         for(let i = 0; i < 10; i++){
             const row = document.createElement("div");
             row.classList.add("rows");
@@ -61,6 +68,7 @@ function makeGrid(){
             }
         } 
     } else {
+        promptText.textContent = `Generating a ${number} x ${number} grid...`;
         for(let i = 0; i < number; i++){
             const row = document.createElement("div");
             row.classList.add("rows");
@@ -76,11 +84,11 @@ function makeGrid(){
 }
 
 
-// makeGrid();
 
 
 clearBtn.addEventListener('click', () =>{
-    //run make grid function passing a 10x10 grid as the arguments
+    input.value = "";
+    makeGrid();
 })
 
 
